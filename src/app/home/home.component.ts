c284eb8021277ce69ca69e4409590b212755018d
Karthik Bhat <karthik.bhat@in.abb.com>
2018-12-07 17:23:10 +0530

    Module-5 checkin

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,30 @@
 import {Component} from '@angular/core';
-
+import { Employee } from '../models/Employee.model'
 @Component({
   selector: 'home',
   styleUrls: ['./home.component.css'],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+  languagesList : string[] = ["English", "German", "Dutch"];
+  model = new Employee('', '', true, '', 'default');
+  isPrimaryLanguageNull: boolean = false;
+
+  validatePrimaryLanguage(event){
+    if(this.model.primaryLanguage === "default"){
+      this.isPrimaryLanguageNull = true;
+    }
+    else{
+      this.isPrimaryLanguageNull = false;
+    }
+  }
+  
+  //can be set/ reset using set and get acessors
+  // _firstName: string = " ";
+  // get firstName(): string{
+  //   return this._firstName
+  // }
+  // set firstName(value){
+  //   this._firstName = value;
+  // }
 }
---
+++ src/app/models/Employee.model.ts
@@ -0,0 +1,10 @@
+export class Employee {
+    constructor(
+        public firstNm: string,
+        public lastNm: string,
+        public isFullTimeEmployee : boolean,
+        public paymentType : string,
+        public primaryLanguage : string
+    ){
+    }
+}
